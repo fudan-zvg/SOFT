42a248998a6d72def088a0497b2941ec02811874
victor <lujiachen@sjtu.edu.cn>
2021-10-19 13:04:16 +0800

    copyright

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# Copyright (c) [2012]-[2021] Shanghai Yitu Technology Co., Ltd.
-#
-# This source code is licensed under the Clear BSD License
-# LICENSE file in the root directory of this file
-# All rights reserved.
-"""
-T2T-ViT training and evaluating script
-This script is modified from pytorch-image-models by Ross Wightman (https://github.com/rwightman/pytorch-image-models/)
-It was started from an early version of the PyTorch ImageNet example
-(https://github.com/pytorch/examples/tree/master/imagenet)
-"""
 import argparse
 import time
 import yaml
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,1 @@
-# Copyright (c) [2012]-[2021] Shanghai Yitu Technology Co., Ltd.
-#
-# This source code is licensed under the Clear BSD License
-# LICENSE file in the root directory of this file
-# All rights reserved.
 from .softmax_free_vision_transformer import *
